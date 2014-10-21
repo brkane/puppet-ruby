948cfbaf72c2cc9937f4c9309644495e8449f942
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-10-21 11:08:11 +0200

    Added: installation of required libraries

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,43 @@
 #
 #   $version - the version as denoted in source filename
 #
+# Dependencies:
+#    autoconf bison build-essential libssl-dev
+#    libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev
 #
 class ruby ($version = '2.1.2') {
+
+  if defined(Package['autoconf']) == false {
+    package { 'autoconf': ensure => present }
+  }
+
+  if defined(Package['bison']) == false {
+    package { 'bison': ensure => present }
+  }
+
+  if defined(Package['build-essential']) == false {
+    package { 'build-essential': ensure => present }
+  }
+
+  if defined(Package['libssl-dev']) == false {
+    package { 'libssl-dev': ensure => present }
+  }
+
+  if defined(Package['libyaml-dev']) == false {
+    package { 'libyaml-dev': ensure => present }
+  }
+
+  if defined(Package['libreadline6-dev']) == false {
+    package { 'libreadline6-dev': ensure => present }
+  }
+
+  if defined(Package['zlib1g-dev']) == false {
+    package { 'zlib1g-dev': ensure => present }
+  }
+
+  if defined(Package['libncurses5-dev']) == false {
+    package { 'libncurses5-dev': ensure => present }
+  }
 
   if defined(Package['curl']) == false {
     package { 'curl': ensure => present }
